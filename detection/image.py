6d3face687ac978ab0d920a8997568abedc0a2e1
minlaxz <mgmglatt.6991@gmail.com>
2020-07-23 02:42:29 +0630

    license plate extracted. next is 4 points perspective.

--- detection/image.py
+++ detection/image.py
@@ -3,38 +3,50 @@ import cv2
 import time
 import darknetanpr as darknet
 
+def breakdown(det):
+    print(det[0].decode())
+    print(det[1])
+    x,y,w,h = det[2]
+    print("x",x)
+    print("y",y)
+    print("w",w)
+    print("h",h)
+
 def convertBack(x, y, w, h):
     xmin = int(round(x - (w / 2)))
     xmax = int(round(x + (w / 2)))
     ymin = int(round(y - (h / 2)))
     ymax = int(round(y + (h / 2)))
+    print(xmin, ymin, xmax, ymax)
     return xmin, ymin, xmax, ymax
 
 
 def cvDrawBoxes(detections, img):
     for detection in detections:
+        breakdown(detection)
         x, y, w, h = detection[2][0],\
             detection[2][1],\
             detection[2][2],\
             detection[2][3]
         xmin, ymin, xmax, ymax = convertBack(
             float(x), float(y), float(w), float(h))
-        pt1 = (xmin, ymin)
-        pt2 = (xmax, ymax)
-        cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
-        cv2.putText(img,
-                    detection[0].decode() +
-                    " [" + str(round(detection[1] * 100, 2)) + "]",
-                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    [0, 255, 0], 2)
-    
-    return img[ymin:ymax, xmin:xmax] if detections else img
-
+        #pt1 = (xmin, ymin)
+        #pt2 = (xmax, ymax)
+        #cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+        #cv2.putText(img,
+        #            detection[0].decode() +
+        #            " [" + str(round(detection[1] * 100, 2)) + "]",
+        #            (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+        #            [0, 255, 0], 2)
+        if detection:
+            cropped = img[ymin:ymax,xmin:xmax]
+            return cropped
+        else
+            return img
 
 netMain = None
 metaMain = None
 altNames = None
-
 
 def YOLO():
 
@@ -76,50 +88,20 @@ def YOLO():
                     pass
         except Exception:
             pass
-    #you will need to start `rtsp server` using god.sh script XD
-    #cap = cv2.VideoCapture('rtsp://user:userpassword@192.168.0.16:8554/anrp_stream')
-    #cap = cv2.VideoCapture("test.mp4")
-    cap = cv2.VideoCapture(0)
-    cap.set(3, darknet.network_height(netMain))
-    cap.set(4, darknet.network_width(netMain))
-    #out = cv2.VideoWriter(
-    #    "output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
-    #    (darknet.network_width(netMain), darknet.network_height(netMain)))
-    print("Starting the YOLO loop...")
 
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
                                     darknet.network_height(netMain),3)
-    while True:
-        #print('fps: ',str(cap.get(cv2.CAP_PROP_FPS)))
-        prev_time = time.time()
-        ret, frame_read = cap.read()
-        frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
-        frame_resized = cv2.resize(frame_rgb,
-                                   (darknet.network_width(netMain),
-                                    darknet.network_height(netMain)),
-                                   interpolation=cv2.INTER_LINEAR)
 
-        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
-
-        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
-#!
-        image = cvDrawBoxes(detections, frame_resized)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#!!
-        #print(1/(time.time()-prev_time))
-        #if (detections == []) :
-        #    pass
-        #else:
-        #    print(detections[0][0])
-        #    print("confidence : ", str(detections[0][1]))
-        #    print("you need to edit these points : " , str(detections[0][2][0]), str(detections[0][2][1]))
-
-        cv2.imshow('Demo', image)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    cap.release()
-    #out.release()
-
+    frame_rgb = cv2.cvtColor(cv2.imread('image.jpg'), cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb,(darknet.network_width(netMain), darknet.network_height(netMain)), interpolation=cv2.INTER_LINEAR)
+    darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+    detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+    image = cvDrawBoxes(detections, frame_resized)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #cv2.imwrite('detected.jpg', image)
+    cv2.imshow('detected.jpg', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() 
 if __name__ == "__main__":
     YOLO()
