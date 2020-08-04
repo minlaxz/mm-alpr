bc656e16ee29aae62d613bdf26f1f4e9a1ca214b
minlaxz <mgmglatt.6991@gmail.com>
2020-08-04 23:16:41 +0630

    constructing single detector.

---
+++ anpr_mm/detector.py
@@ -0,0 +1,229 @@
+#!$HOME/miniconda3/envs/tfk/bin/python
+"""
+@author: Philip Kahn
+@date: 20180503
+@forkFrom: https://github.com/AlexeyAB/darknet/blob/master/darknet.py
+@CAPI: https://github.com/AlexeyAB/darknet/blob/master/include/darknet.h
+@authorOfYolo: https://pjreddie.com/darknet
+@adaptedProject: (TTU-2019-2020-EC)-Final Year Project-Group-22
+## Adapted with ImageZMQ
+This file needs resized RGB frame and return detected image if detection else None
+"""
+import os
+import cv2
+from ctypes import *
+hasGPU=True
+
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+class DETECTION(Structure):
+    _fields_ = [("bbox", BOX),
+                ("classes", c_int),
+                ("prob", POINTER(c_float)),
+                ("mask", POINTER(c_float)),
+                ("objectness", c_float),
+                ("sort_class", c_int),
+                ("uc", POINTER(c_float)),
+                ("points", c_int)]
+
+class DETNUMPAIR(Structure):
+    _fields_ = [("num", c_int),
+                ("dets", POINTER(DETECTION))]
+
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+
+class METADATA(Structure):
+    _fields_ = [("classes", c_int),
+                ("names", POINTER(c_char_p))]
+    
+
+lib = CDLL("./detection/libdarknet.so", RTLD_GLOBAL)
+lib.network_width.argtypes = [c_void_p]
+lib.network_width.restype = c_int
+lib.network_height.argtypes = [c_void_p]
+lib.network_height.restype = c_int
+
+#copy image from bytes
+copy_image_from_bytes = lib.copy_image_from_bytes
+copy_image_from_bytes.argtypes = [IMAGE,c_char_p]
+
+
+#width & height
+def network_width(net):
+    return lib.network_width(net)
+def network_height(net):
+    return lib.network_height(net)
+
+if hasGPU:
+    set_gpu = lib.cuda_set_device
+    set_gpu.argtypes = [c_int]
+
+#init cpu
+init_cpu = lib.init_cpu
+
+#make image
+make_image = lib.make_image
+make_image.argtypes = [c_int, c_int, c_int]
+make_image.restype = IMAGE
+
+#load net custom
+load_net_custom = lib.load_network_custom
+load_net_custom.argtypes = [c_char_p, c_char_p, c_int, c_int]
+load_net_custom.restype = c_void_p
+
+#load meta
+load_meta = lib.get_metadata
+lib.get_metadata.argtypes = [c_char_p]
+lib.get_metadata.restype = METADATA
+
+#get network boxes
+get_network_boxes = lib.get_network_boxes
+get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int), c_int]
+get_network_boxes.restype = POINTER(DETECTION)
+
+#free detections
+free_detections = lib.free_detections
+free_detections.argtypes = [POINTER(DETECTION), c_int]
+
+#goddamn_predictor
+predict_image = lib.network_predict_image
+predict_image.argtypes = [c_void_p, IMAGE]
+predict_image.restype = POINTER(c_float)
+
+#do_nms_sort
+do_nms_sort = lib.do_nms_sort
+do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+def detect_image(net, meta, im, thresh=.5, hier_thresh=.5, nms=.45, debug= False):
+    num = c_int(0)
+    if debug: print("Assigned num")
+    pnum = pointer(num)
+    if debug: print("Assigned pnum")
+    predict_image(net, im)
+    letter_box = 0
+    #predict_image_letterbox(net, im)
+    #letter_box = 1
+    if debug: print("did prediction")
+    #dets = get_network_boxes(net, custom_image_bgr.shape[1], custom_image_bgr.shape[0], thresh, hier_thresh, None, 0, pnum, letter_box) # OpenCV
+    dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0, pnum, letter_box)
+    if debug: print("Got dets")
+    num = pnum[0]
+    if debug: print("got zeroth index of pnum")
+    if nms:
+        do_nms_sort(dets, num, meta.classes, nms)
+    if debug: print("did sort")
+    res = []
+    if debug: print("about to range")
+    for j in range(num):
+        if debug: print("Ranging on "+str(j)+" of "+str(num))
+        if debug: print("Classes: "+str(meta), meta.classes, meta.names)
+        for i in range(meta.classes):
+            if debug: print("Class-ranging on "+str(i)+" of "+str(meta.classes)+"= "+str(dets[j].prob[i]))
+            if dets[j].prob[i] > 0:
+                b = dets[j].bbox
+                if altNames is None:
+                    nameTag = meta.names[i]
+                else:
+                    nameTag = altNames[i]
+                if debug:
+                    print("Got bbox", b)
+                    print(nameTag)
+                    print(dets[j].prob[i])
+                    print((b.x, b.y, b.w, b.h))
+                res.append((nameTag, dets[j].prob[i], (b.x, b.y, b.w, b.h)))
+    if debug: print("did range")
+    res = sorted(res, key=lambda x: -x[1])
+    if debug: print("did sort")
+    free_detections(dets, num)
+    if debug: print("freed detections")
+    return res
+
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+def cropDetected(detections, img):
+    for d in detections:
+        xmin, ymin, xmax, ymax = convertBack(float(d[2][0]), float(d[2][1]), float(d[2][2]), float(d[2][3]))
+        return img[ymin:ymax, xmin:xmax] # if detections else None
+
+netMain = None
+metaMain = None
+altNames = None
+
+def initialize_darknet():
+    global metaMain, netMain, altNames
+    configPath = "./detection/network/yolov3-tiny_obj.cfg"
+    weightPath = "./detection/network/yolov3-tiny_obj_best.weights"
+    metaPath = "./detection/network/obj.data"
+    if not os.path.exists(configPath):
+        raise ValueError("Invalid config path `" +
+                         os.path.abspath(configPath)+"`")
+    if not os.path.exists(weightPath):
+        raise ValueError("Invalid weight path `" +
+                         os.path.abspath(weightPath)+"`")
+    if not os.path.exists(metaPath):
+        raise ValueError("Invalid data file path `" +
+                         os.path.abspath(metaPath)+"`")
+    if netMain is None:
+        netMain = darknet.load_net_custom(configPath.encode(
+            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+    if metaMain is None:
+        metaMain = darknet.load_meta(metaPath.encode("ascii"))
+    if altNames is None:
+        try:
+            with open(metaPath) as metaFH:
+                metaContents = metaFH.read()
+                import re
+                match = re.search("names *= *(.*)$", metaContents,
+                                  re.IGNORECASE | re.MULTILINE)
+                if match:
+                    result = match.group(1)
+                else:
+                    result = None
+                try:
+                    if os.path.exists(result):
+                        with open(result) as namesFH:
+                            namesList = namesFH.read().strip().split("\n")
+                            altNames = [x.strip() for x in namesList]
+                except TypeError:
+                    pass
+        except Exception:
+            pass
+count = 0
+def YOLO(frame_rgb):
+    """ ("Starting the YOLO loop") """
+    global count
+    # Create an image we reuse for each detect
+    darknet_image = darknet.make_image(darknet.network_width(netMain),
+                                    darknet.network_height(netMain),3)
+    frame_resized = cv2.resize(frame_rgb,
+                                   (darknet.network_width(netMain),
+                                    darknet.network_height(netMain)),
+                                   interpolation=cv2.INTER_LINEAR) # image is already resized by sender (client) (512, 416)
+    darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+    detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.75)
+    if detections:
+        if detections[0][1] > 0.95:
+            count += 1
+            print('detected:',str(count))
+            if count >= 10:
+                return (detections)
+            else: return (None)
+        else: 
+            count = count - 3 #rarely 
+            return (None)
+    else:
+        count = 0
+        return (None)
