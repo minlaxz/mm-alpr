70fcc162ab6aa1f46ee969d6f0c7554c577fccf8
minlaxz <mgmglatt.6991@gmail.com>
2020-08-05 18:50:21 +0630

    imageZMQ sender is better now.

--- anpr_mm/sender.py
+++ anpr_mm/sender.py
@@ -2,56 +2,82 @@
 File - : __file__
 This should be run on Client Side : Raspberry PI
 """
+
 import os, imagezmq
-from socket import gethostname as clienthostname
+from socket import gethostname as name           
+import cv2
+import sys
+import traceback
+import time
+import logging
+
+class Master:
+    def __init__(self, debug=False):
+        format = "%(asctime)s: %(message)s"
+        logging.basicConfig(format=format, level=logging.INFO,
+                            datefmt="%H:%M:%S")
+        self.debug = debug
+        self.is_x64 = False if 'arm' in os.uname()[-1] else True
+
+        if self.debug: logging.info('`Master Constructor` : X64 device.') if self.is_x64 else logging.info('Master Constructor : X64 device.')
+        self.userAbord = False
+        self.count = 1
+    
+    def connect_server(self, server_host):
+        self.server_host = server_host
+        if self.debug : logging.info('`connect_server method` : Image Server is tcp/' + str(self.server_host))
+        connect_to = 'tcp://{}:5555'.format(self.server_host)
+        self.image_sender = imagezmq.ImageSender(connect_to=connect_to)
+        if self.debug: logging.info('`connect_server method` : Connected to the server.')
+    
+    def send_frame(self):
+        try:
+            while True:
+                if self.is_x64: 
+                    _, image = self.camera.read()
+                else: image = self.camera.read()
+                resized_rgb = cv2.resize(
+                            cv2.cvtColor(image, cv2.COLOR_BGR2RGB),(w,h),
+                            interpolation=cv2.INTER_LINEAR)
+                repl = self.image_sender.send_image(name(), resized_rgb)
+                self.count += 1 
+                self.debug: logging.info('`send_frame method` : {0}, {1}'. format(self.count, repl))
+                if (repl==b'STOP'):
+                    self.userAbord = True
+                    break
+        except (KeyboardInterrupt, SystemExit):
+            logging.info('Keyboard is Interrupted')
+            pass  # Ctrl-C was pressed to end program
+        except Exception as e:
+            logging.info('Python error with no Exception handler:')
+            logging.info('Traceback error:', e)
+            traceback.print_exc()
+        finally:
+            logging.info('STOP SIGNAL') if self.userAbord else logging.info('Finally Exit')
+            self.camera.release() if self.is_x64 else self.camera.stop()
+            if self.debug: logging.info('Released Camera Object.')
+            self.image_sender.close()
+            if self.debug: logging.info('Closed Connection')
+            sys.exit()
+
+    def camera_instruct(self):
+        if self.debug: logging.info('`camrea_instruct method` : Creating Camera OBJECT.')
+        if self.is_x64:
+            self.camera = cv2.VideoCapture(0)
+            self.camera.set(3,h)
+            self.camera.set(4,w)
+        else:
+            from imutils import VideoStream
+            self.camera = VideoStream(usePiCamera=True, resolution=(w,h)).start()
+        if self.debug: logging.info('`camera_instruct method` : Camera is warming up.')
+        time.sleep(2.0)
+        logging.info('`camera_instruct method` : `{}` Camera is start.'.format(name()))
+
 debug = False
-server_host = '192.168.0.16'
 w, h = 512, 416
+app = Master(debug=debug)
+if debug: logging.info('`Main Thread` : Image Sender is initialized.')
+app.camera_instruct()
+app.connect_server('192.168.0.16')
+app.send_frame()
 
-image_sender = imagezmq.ImageSender(connect_to='tcp://{}:5555'.format(server_host))
-is_x64 = None
-if 'arm' in os.uname()[-1]:
-    from imutils.video import VideoStream
-    import os, sys, traceback, time
-    is_x64 = False 
-else:
-    import cv2
-    from sys import exit
-    from traceback import print_exc
-    from time import sleep
-    is_x64 = True
-if is_x64:
-    camera = cv2.VideoCapture(0)
-    camera.set(3,h)
-    camera.set(4,w)
-else:
-    camera = VideoStream(usePiCamera=True, resolution=(w,h)).start()
-
-sleep(2.0) if is_x64 else time.sleep(2.0)
-print('`{}` Camera is start.'.format(clienthostname()))
-userAbord = False
-try:
-    while True:
-        if is_x64:
-            _, image = camera.read()
-        else: image = camera.read()
-        repl = image_sender.send_image(clienthostname(), cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        if (repl==b'STOP'):
-            userAbord = True
-            break
-
-except (KeyboardInterrupt, SystemExit):
-    print('KeyboardInterrupt')
-    pass  # Ctrl-C was pressed to end program
-
-except Exception as ex:
-    print('ex')
-    print('Python error with no Exception handler:')
-    print('Traceback error:', ex)
-    traceback.print_exc()
-
-finally:
-    print('User Stopped') if userAbord else print('Finally Exit')
-    camera.release() if is_x64 else camera.stop()
-    image_sender.close()
-    exit() if is_x64 else sys.exit()
