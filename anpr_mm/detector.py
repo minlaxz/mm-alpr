1c1eca22828757f246a5a8d0aa38b250eb1d750c
minlaxz <minminlaxz@gmail.com>
2020-09-05 01:26:19 +0630

    detector enhanced.

--- anpr_mm/detector.py
+++ anpr_mm/detector.py
@@ -5,14 +5,15 @@
 @forkFrom: https://github.com/AlexeyAB/darknet/blob/master/darknet.py
 @CAPI: https://github.com/AlexeyAB/darknet/blob/master/include/darknet.h
 @authorOfYolo: https://pjreddie.com/darknet
-@adaptedProject: (TTU-2019-2020-EC)-Final Year Project-Group-22
-## Adapted with ImageZMQ
-This file needs resized RGB frame and return detected image if detection else None
+# adaptedProject: (TTU-2019-2020-EC)-Final Year Project-Group-22
+# https://github.com/minlaxz/darkent-thesisz
+# Adapted with ImageZMQ
+# This must be supplied resized RGB frame and return detected image if detection else None
 """
 from ctypes import *
 import os
-
-hasGPU=True
+import random
+import math
 
 class BOX(Structure):
     _fields_ = [("x", c_float),
@@ -47,7 +48,122 @@ class IMAGE(Structure):
 class METADATA(Structure):
     _fields_ = [("classes", c_int),
                 ("names", POINTER(c_char_p))]
-    
+
+def class_colors(names):
+    """
+    Create a dict with one random BGR color for each
+    class name
+    """
+    return {name: (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255)) for name in names}
+
+def decode_detection(detections):
+    decoded = []
+    for label, confidence, bbox in detections:
+        confidence = str(round(confidence * 100, 2))
+        decoded.append((str(label), confidence, bbox))
+    return decoded
+
+
+def remove_negatives(detections, class_names, num):
+    """
+    Remove all classes with 0% confidence within the detection
+    """
+    predictions = []
+    for j in range(num):
+        for idx, name in enumerate(class_names):
+            if detections[j].prob[idx] > 0:
+                bbox = detections[j].bbox
+                bbox = (bbox.x, bbox.y, bbox.w, bbox.h)
+                predictions.append((name, detections[j].prob[idx], (bbox)))
+    return predictions
+
+class NetworkParameters:
+    def __init__(self, _config, _data, _weights, _thresh):
+        self.config_file = _config
+        self.data_file = _data
+        self.weights = _weights
+        self.thresh  = _thresh
+
+    def check_arguments_errors(self):
+        assert 0 < self.thresh < 1, "Threshold should be a float between zero and one (non-inclusive)"
+        if not os.path.exists(self.config_file):
+            raise(ValueError("Invalid config path {}".format(os.path.abspath(args.config_file))))
+        if not os.path.exists(self.weights):
+            raise(ValueError("Invalid weight path {}".format(os.path.abspath(args.weights))))
+        if not os.path.exists(self.data_file):
+            raise(ValueError("Invalid data file path {}".format(os.path.abspath(args.data_file))))
+
+class LoadNetwork(NetworkParameters):
+    def __init__(self, config_file, data_file, weights, thresh=0.5 , batch_size=1):
+        super().__init__(config_file, data_file, weights, thresh)
+        self.check_arguments_errors()
+        
+        self.hier_thresh = .5
+        self.nms = .45
+        self.thresh = thresh
+
+        debug = False
+        
+        self.network = load_net_custom(
+            self.config_file.encode("ascii"),
+            self.weights.encode("ascii"), 0, batch_size)
+
+        metadata = load_meta(self.data_file.encode("ascii"))
+        self.class_names = [metadata.names[i].decode("ascii") for i in range(metadata.classes)]
+        self.class_colors = class_colors(self.class_names)
+        self.network_w = lib.network_width(self.network)
+        self.network_h = lib.network_height(self.network)
+
+        # return network, class_names, colors
+
+    def detect_image(self, resized_rgb):
+        """
+        Returns a list with highest confidence class and their bbox
+        """
+        self.dk_image = make_image(self.network_w, self.network_h, 3)
+        copy_image_from_bytes(self.dk_image,resized_rgb.tobytes())
+
+        pnum = pointer(c_int(0))
+        predict_image(self.network, self.dk_image)
+        detections = get_network_boxes(self.network, self.dk_image.w, self.dk_image.h,
+                        self.thresh, self.hier_thresh, None, 0, pnum, 0)
+        num = pnum[0]
+        if self.nms:
+            do_nms_sort(detections, num, len(self.class_names), self.nms)
+        predictions = remove_negatives( detections, self.class_names, num)
+        predictions = decode_detection(predictions)
+        free_detections(detections, num)
+        free_image(self.dk_image)
+        return sorted(predictions, key=lambda x: x[1])
+
+count = 0
+def YOLO(resized_rgb):
+    """ needs resized rgb frame """
+    global count
+
+    darknet_image = make_image(network_width(netMain),network_height(netMain),3)
+    """ image is already resized and rgb by sender (client) (w512, h416) """
+    copy_image_from_bytes(darknet_image,resized_rgb.tobytes())
+
+    detections = detect_image(netMain, metaMain, darknet_image, thresh=0.75)
+    if detections:
+        if detections[0][1] > 0.95:
+            count += 1
+            print('detected:',str(count))
+            if count >= 10:
+                return (detections)
+            else: return (None)
+        else: 
+            count = count - 3 #rarely 
+            return (None)
+    else:
+        count = 0
+        return (None)
+
+hasGPU=True
 
 lib = CDLL("./libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
@@ -58,14 +174,6 @@ lib.network_height.restype = c_int
 #copy image from bytes
 copy_image_from_bytes = lib.copy_image_from_bytes
 copy_image_from_bytes.argtypes = [IMAGE,c_char_p]
-
-
-#width & height
-def network_width(net):
-    return lib.network_width(net)
-
-def network_height(net):
-    return lib.network_height(net)
 
 if hasGPU:
     set_gpu = lib.cuda_set_device
@@ -113,114 +221,6 @@ predict_image.restype = POINTER(c_float)
 do_nms_sort = lib.do_nms_sort
 do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
 
-netMain = None
-metaMain = None
-altNames = None
+do_nms_obj = lib.do_nms_obj
+do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
 
-def initialize_darknet():
-    global metaMain, netMain, altNames
-    configPath = "./network/yolov3-tiny_obj.cfg"
-    weightPath = "./network/yolov3-tiny_obj_best.weights"
-    metaPath = "./network/obj.data"
-    if not os.path.exists(configPath):
-        raise ValueError("Invalid config path `" +
-                         os.path.abspath(configPath)+"`")
-    if not os.path.exists(weightPath):
-        raise ValueError("Invalid weight path `" +
-                         os.path.abspath(weightPath)+"`")
-    if not os.path.exists(metaPath):
-        raise ValueError("Invalid data file path `" +
-                         os.path.abspath(metaPath)+"`")
-    if netMain is None:
-        netMain = load_net_custom(configPath.encode(
-            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
-    if metaMain is None:
-        metaMain = load_meta(metaPath.encode("ascii"))
-    if altNames is None:
-        try:
-            with open(metaPath) as metaFH:
-                metaContents = metaFH.read()
-                import re
-                match = re.search("names *= *(.*)$", metaContents,
-                                  re.IGNORECASE | re.MULTILINE)
-                if match:
-                    result = match.group(1)
-                else:
-                    result = None
-                try:
-                    if os.path.exists(result):
-                        with open(result) as namesFH:
-                            namesList = namesFH.read().strip().split("\n")
-                            altNames = [x.strip() for x in namesList]
-                except TypeError:
-                    pass
-        except Exception:
-            pass
-        
-def detect_image(net, meta, im, thresh=.5, hier_thresh=.5, nms=.45, debug= False):
-    num = c_int(0)
-    if debug: print("Assigned num")
-    pnum = pointer(num)
-    if debug: print("Assigned pnum")
-    predict_image(net, im)
-    letter_box = 0
-    #predict_image_letterbox(net, im)
-    #letter_box = 1
-    if debug: print("did prediction")
-    #dets = get_network_boxes(net, custom_image_bgr.shape[1], custom_image_bgr.shape[0], thresh, hier_thresh, None, 0, pnum, letter_box) # OpenCV
-    dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0, pnum, letter_box)
-    if debug: print("Got dets")
-    num = pnum[0]
-    if debug: print("got zeroth index of pnum")
-    if nms:
-        do_nms_sort(dets, num, meta.classes, nms)
-    if debug: print("did sort")
-    res = []
-    if debug: print("about to range")
-    for j in range(num):
-        if debug: print("Ranging on "+str(j)+" of "+str(num))
-        if debug: print("Classes: "+str(meta), meta.classes, meta.names)
-        for i in range(meta.classes):
-            if debug: print("Class-ranging on "+str(i)+" of "+str(meta.classes)+"= "+str(dets[j].prob[i]))
-            if dets[j].prob[i] > 0:
-                b = dets[j].bbox
-                if altNames is None:
-                    nameTag = meta.names[i]
-                else:
-                    nameTag = altNames[i]
-                if debug:
-                    print("Got bbox", b)
-                    print(nameTag)
-                    print(dets[j].prob[i])
-                    print((b.x, b.y, b.w, b.h))
-                res.append((nameTag, dets[j].prob[i], (b.x, b.y, b.w, b.h)))
-    if debug: print("did range")
-    res = sorted(res, key=lambda x: -x[1])
-    if debug: print("did sort")
-    free_detections(dets, num)
-    if debug: print("freed detections")
-    return res
-
-count = 0
-def YOLO(resized_rgb):
-    """ needs resized rgb frame """
-    global count
-
-    darknet_image = make_image(network_width(netMain),network_height(netMain),3)
-    """ image is already resized and rgb by sender (client) (w512, h416) """
-    copy_image_from_bytes(darknet_image,resized_rgb.tobytes())
-
-    detections = detect_image(netMain, metaMain, darknet_image, thresh=0.75)
-    if detections:
-        if detections[0][1] > 0.95:
-            count += 1
-            print('detected:',str(count))
-            if count >= 10:
-                return (detections)
-            else: return (None)
-        else: 
-            count = count - 3 #rarely 
-            return (None)
-    else:
-        count = 0
-        return (None)
