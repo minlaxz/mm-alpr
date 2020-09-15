b8fb748f3ac270eaa2285a0a52d4e4f5bf4e05eb
minlaxz <minminlaxz@gmail.com>
2020-09-15 15:53:51 +0630

    update libraries to ours.

--- anpr_mm/sender.py
+++ anpr_mm/sender.py
@@ -4,12 +4,13 @@ This should be run on Client Side : Raspberry PI or PC like x64
 Adapted with Windows OS
 """
 
-import os, imagezmq
-from socket import gethostname as name           
+import os
+from socket import gethostname
 import cv2
 import traceback
 import time
-import logging
+import logxs as log
+import imzmqx
 
 class Master:
     def __init__(self, debug=False, test=False, server=None):
@@ -20,64 +21,63 @@ class Master:
         self.userAbord = False
         self.count = 1
 
-        format = "%(asctime)s: %(message)s"
-        logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
+        # format = "%(asctime)s: %(message)s"
+        # logging.basicConfig(format=format, level=log.this, datefmt="%H:%M:%S")
         
-        if self.debug: logging.debug('platform : posix .') if self.is_posix else logging.debug('platform : nt .')
-        logging.debug('Ready.')
+        if self.debug: log.this('platform : posix .') if self.is_posix else log.this('platform : nt .')
+        log.this('Ready.')
     
     def run(self):
         self.camera_instruct()
-        logging.debug('`camera_instruct` : `{}` Camera is start.'.format(name()))
+        log.this('`camera_instruct` : `{}` Camera is start.'.format(name()))
         self.connect_server()
-        if self.debug: logging.debug('Connected to the server.')
+        if self.debug: log.this('Connected to the server.')
         self.send_frame()
     
     def connect_server(self):
-        if self.debug : logging.debug('Server is tcp/' + str(self.server_host))
+        if self.debug : log.this('Server is tcp/' + str(self.server_host))
         connect_to = 'tcp://{}:5555'.format(self.server_host)
-        self.image_sender = imagezmq.ImageSender(connect_to=connect_to)
-        if self.debug: logging.debug('Image sender is inittialized.')
+        self.image_sender = imzmqx.ImageSender(connect_to=connect_to)
+        if self.debug: log.this('Image sender is inittialized.')
 
     
     def send_frame(self):
-        if self.debug: logging.debug('Handling over frame sending method.')
+        if self.debug: log.this('Handling over frame sending method.')
         try:
             while True:
                 _ , image = self.camera.read()
 
-                resized_rgb = image if self.test else cv2.resize(
-                                                                cv2.cvtColor(image, cv2.COLOR_BGR2RGB),(w,h),
+                resized_rgb = image if self.test else cv2.resize( cv2.cvtColor(image, cv2.COLOR_BGR2RGB),(w,h),
                                                                 interpolation=cv2.INTER_LINEAR)
 
-                repl = self.image_sender.send_image(name(), resized_rgb)
+                repl = self.image_sender.send_image( gethostname() , resized_rgb)
                 self.count += 1 
-                if self.debug: logging.debug('`send_frame method` : {0}, {1}'. format(self.count, repl))
+                if self.debug: log.this('`send_frame method` : {0}, {1}'. format(self.count, repl))
                 if (repl==b'STOP'):
                     self.userAbord = True
                     break
         except (KeyboardInterrupt, SystemExit):
-            logging.debug('Keyboard is Interrupted')
+            log.this('Keyboard is Interrupted')
             pass  # Ctrl-C was pressed to end program
         except Exception as e:
-            logging.debug('error with no Exception handler:')
-            logging.debug('Traceback error:', e)
+            log.this('error with no Exception handler:')
+            log.this('Traceback error:', e)
             traceback.print_exc()
         finally:
-            logging.debug('got STOP SIGNAL') if self.userAbord else logging.debug('Finally Exit')
+            log.this('got STOP SIGNAL') if self.userAbord else log.this('Finally Exit')
             self.camera.release()
-            if self.debug: logging.debug('Released Camera Object.')
+            if self.debug: log.this('Released Camera Object.')
             self.image_sender.close()
-            if self.debug: logging.debug('Closed Connection')
-            logging.debug('Done.')
+            if self.debug: log.this('Closed Connection')
+            log.this('Done.')
 
     def camera_instruct(self):
-        if self.debug: logging.debug('Creating Camera OBJECT.')
+        if self.debug: log.this('Creating Camera OBJECT.')
         self.camera = cv2.VideoCapture(0)
         self.camera.set(3,h)
         self.camera.set(4,w)
-        if self.debug: logging.debug('set frame dimention: w{},h{}'.format(w,h))
-        logging.debug('Camera is warming up.')
+        if self.debug: log.this('set frame dimention: w{},h{}'.format(w,h))
+        log.this('Camera is warming up.')
         time.sleep(3.0)
 
 w, h = 512, 416
