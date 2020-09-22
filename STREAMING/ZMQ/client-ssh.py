b545326dd1b85ca946378516ce64dbe88c3042cb
minlaxz <minminlaxz@gmail.com>
2020-09-22 18:28:06 +0630

    ZMQ tests.

---
+++ STREAMING/ZMQ/client-ssh.py
@@ -0,0 +1,19 @@
+from zmq import ssh
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+
+ssh.tunnel_connection(socket , "tcp://localhost:5555", "tcp://localhost")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(10):
+    print("Sending request %s …" % request)
+    socket.send(b"Hello")
+
+    #  Get the reply.
+    message = socket.recv()
+    print("Received reply %s [ %s ]" % (request, message))
