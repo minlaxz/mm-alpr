39fa062332b15b64ab71ba9860a8c2dd3ea1b6a4
minlaxz <minminlaxz@gmail.com>
2020-09-22 20:00:46 +0630

    code emphasized.

--- LOGGER/log_maker.py
+++
@@ -1,9 +0,0 @@
-import logging
-def logger(logtext):
-    log = "makelog.txt"
-    logging.basicConfig(filename=log, level=logging.DEBUG,
-                        format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
-    logging.info(logtext)
-
-if __name__=="__main__":
-    logger('text')
