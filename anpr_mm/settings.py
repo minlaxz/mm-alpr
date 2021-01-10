85c5eb9661bf2a9ced7f1cb6f95b73dada9206d1
minlaxz <minminlaxz@gmail.com>
2021-01-11 02:52:19 +0630

    Restructing Everything.

--- anpr_mm/settings.py
+++ anpr_mm/settings.py
@@ -1,54 +1,68 @@
 from configparser import SafeConfigParser
-from . import log
-import os.path
-cfg = "./anpr_mm.cfg"
-safe = True
-if (not os.path.exists(cfg)):
-    log.this("settingparser: config file not found!")
-    safe = False
+import pylaxz
+import os
+
+cfg = "./settings/config.ini"
+
+if not os.path.exists(cfg):
+    pylaxz.printf("config file not found!", _int=True, _err=True)
+
 
 class Settings:
     def __init__(self):
         self.parser = SafeConfigParser()
+        self.parser.read(cfg)
+        self.debug = True if (self.parser.get("global", "debug") in ["True"]) else False
+        self.test = True if (self.parser.get("global", "test") in ["True"]) else False
+        self.localrun = (
+            True if (self.parser.get("global", "localrun") in ["True"]) else False
+        )
+        self.gui = True if (self.parser.get("mode", "gui") in ["True"]) else False
 
-    def read(self):
-        if safe:
-            self.parser.read(cfg)
-            self.debug = True if (self.parser.get('global','debug') in ['True']) else False
-            self.test = True if (self.parser.get('global','test') in ['True']) else False
-            self.localrun = True if (self.parser.get('global','localrun') in ['True']) else False
-            self.gui = True if (self.parser.get('mode', 'gui') in ['True']) else False
-        else:
-            log.this("settingparser: read - skipping.")
+    @property
+    def configs(self):
+        return self
+
+    @configs.setter
+    def configs(self, arg):
+        self.test = arg
+
+    @classmethod
+    def callclass(cls):
+        pass
 
     def edit(self):
-        u = input('Debug : `{}` [c]hange : '.format(self.debug))
-        if ('c' in u): self.parser.set('global','debug',str(not self.debug))
-        u = input('Test : `{}` [c]hange : '.format(self.test))
-        if ('c' in u): self.parser.set('global','debug',str(not self.test))
-        u = input('Localrun : `{}` [c]hange : '.format(self.localrun))
-        if ('c' in u): self.parser.set('global','debug',str(not self.localrun))
-        if ('a' in input('[a]pply? : ')):
+        u = input("Debug : `{}` [c]hange : ".format(self.debug))
+        if "c" in u:
+            self.parser.set("global", "debug", str(not self.debug))
+        u = input("Test : `{}` [c]hange : ".format(self.test))
+        if "c" in u:
+            self.parser.set("global", "debug", str(not self.test))
+        u = input("Localrun : `{}` [c]hange : ".format(self.localrun))
+        if "c" in u:
+            self.parser.set("global", "debug", str(not self.localrun))
+        if "a" in input("[a]pply? : "):
             self.apply()
         else:
-            log.this('cancled.')
+            pylaxz.printf("cancled.", _int=True)
 
     def apply(self):
-        with open(cfg, 'w') as f:
+        with open(cfg, "w") as f:
             self.parser.write(f)
-        log.this('settings applied.')
+        pylaxz.printf("settings applied.", _int=True)
+
 
 if __name__ == "__main__":
-    log.this("Runninng standalone.")
-    activity = Settings()
-    activity.read()
-    log.this("Debug: `{}`".format(activity.debug))
-    log.this("Test: `{}` Detection or Not".format(activity.test))
-    log.this("Localrun: `{}` Local Camera or TCP Camera".format(activity.localrun))
-    u = input('Edit? T/F : ')
-    if (u == "T"):
-        activity.edit()
-    elif(u == "F"):
-        pass
-    else:
-        log.this("either T or F !!")
+    s = Settings()
+    pylaxz.printf(s.configs.test, _int=True)
+
+    s.configs = True
+    pylaxz.printf(s.configs.test, _int=True)
+
+
+
+    # pylaxz.printf("Debug: {}".format(activity.debug), _int=True)
+    # pylaxz.printf("Test: {} Detection or Not".format(activity.test), _int=True)
+    # pylaxz.printf(
+    #     "Localrun: {} Local Camera or TCP Camera".format(activity.localrun), _int=True
+    # )
