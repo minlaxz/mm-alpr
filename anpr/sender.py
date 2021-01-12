e0d0a3e26073c4bb9c26038127f7cf35ca17a94a
minlaxz <minminlaxz@gmail.com>
2021-01-12 19:58:50 +0630

    Sender is good

--- anpr/sender.py
+++ anpr/sender.py
@@ -1,85 +1,75 @@
 """
 File - : __file__
-This should be run on Client Side : Raspberry PI or PC like x64
-Adapted with Windows OS
+This should be run on Client Side : Raspberry PI or like Low Power PC
+
+Example:
+    app = Sender()
+    app.run()
 """
 
-import os
+import time, imzmqx, traceback
 from socket import gethostname
 import cv2
-import traceback
-import time
-import logxs as log
-import imzmqx
+import pylaxz
+from configs.settings import Settings
 
-class Master:
-    def __init__(self, debug=False, test=False, server=None):
-        self.debug = debug
-        self.test = test
-        self.server_host = server if server else 'localhost'
-        self.is_posix = True if os.name is 'posix' else False
-        self.userAbord = False
-        self.count = 1
 
-        # format = "%(asctime)s: %(message)s"
-        # logging.basicConfig(format=format, level=log.this, datefmt="%H:%M:%S")
-        
-        if self.debug: log.this('platform : posix .') if self.is_posix else log.this('platform : nt .')
-        log.this('Ready.')
-    
+class Sender:
+    def __init__(self):
+        self.size = {'w':512, 'h':416}
+        s = Settings()
+        self.debug, self.nodetect, self.hostcamera, self.gui = s.appconfigs
+        self.server = Settings.get_server().parser.get("server", "host")
+
     def run(self):
-        self.camera_instruct()
-        log.this('`camera_instruct` : `{}` Camera is start.'.format(name()))
+        self.init_camera()
         self.connect_server()
-        if self.debug: log.this('Connected to the server.')
         self.send_frame()
-    
-    def connect_server(self):
-        if self.debug : log.this('Server is tcp/' + str(self.server_host))
-        connect_to = 'tcp://{}:5555'.format(self.server_host)
-        self.image_sender = imzmqx.ImageSender(connect_to=connect_to)
-        if self.debug: log.this('Image sender is inittialized.')
 
-    
+    def connect_server(self):
+        if self.debug:
+            pylaxz.printf("Server is tcp/" + str(self.server))
+        connect_to = "tcp://{}:5555".format(self.server)
+        self.image_sender = imzmqx.ImageSender(connect_to=connect_to)
+        if self.debug:
+            pylaxz.printf("Waiting to upload...", _int=True)
+
     def send_frame(self):
-        if self.debug: log.this('Handling over frame sending method.')
         try:
             while True:
-                _ , image = self.camera.read()
+                _, image = self.camera.read()
+                resized_rgb = image if self.test else cv2.resize( cv2.cvtColor(image, cv2.COLOR_BGR2RGB), (self.size['w'], self.size['h']), interpolation=cv2.INTER_LINEAR)
 
-                resized_rgb = image if self.test else cv2.resize( cv2.cvtColor(image, cv2.COLOR_BGR2RGB),(w,h),
-                                                                interpolation=cv2.INTER_LINEAR)
-
-                repl = self.image_sender.send_image( gethostname() , resized_rgb)
-                self.count += 1 
-                if self.debug: log.this('`send_frame method` : {0}, {1}'. format(self.count, repl))
-                if (repl==b'STOP'):
+                repl = self.image_sender.send_image(gethostname(), resized_rgb)
+                self.count += 1
+                if self.debug:
+                    pylaxz.printf("`frame is sent, ` count : {0}, reply :{1}".format(self.count, repl))
+                if repl == b"STOP":
                     self.userAbord = True
                     break
         except (KeyboardInterrupt, SystemExit):
-            log.this('Keyboard is Interrupted')
+            pylaxz.printf("Keyboard is Interrupted", _int=1)
             pass  # Ctrl-C was pressed to end program
         except Exception as e:
-            log.this('error with no Exception handler:')
-            log.this('Traceback error:', e)
+            pylaxz.printf("error with no Exception handler:", _int=1)
+            print("Traceback error:", e)
             traceback.print_exc()
         finally:
-            log.this('got STOP SIGNAL') if self.userAbord else log.this('Finally Exit')
+            pylaxz.printf("--STOP SIGNAL--", _int=True) if self.userAbord else pylaxz.pritnf("Exiting", _int=True)
             self.camera.release()
-            if self.debug: log.this('Released Camera Object.')
+            if self.debug:
+                pylaxz.printf("Released Camera Object.", _int=1)
             self.image_sender.close()
-            if self.debug: log.this('Closed Connection')
-            log.this('Done.')
+            if self.debug:
+                pylaxz.printf("Closed Connection", _int=1)
 
-    def camera_instruct(self):
-        if self.debug: log.this('Creating Camera OBJECT.')
+    def init_camera(self):
         self.camera = cv2.VideoCapture(0)
-        self.camera.set(3,h)
-        self.camera.set(4,w)
-        if self.debug: log.this('set frame dimention: w{},h{}'.format(w,h))
-        log.this('Camera is warming up.')
+        self.camera.set(3, self.size['h'])
+        self.camera.set(4, self.size['h'])
+        pylaxz.printf("Camera is startup up, please wait.", _int=True)
         time.sleep(3.0)
 
-w, h = 512, 416
-app = Master(debug=True, test=False, server='192.168.0.16')
-app.run()
+if __name__ == "__main__":
+    app = Sender()
+    app.run() if not app.hostcamera else exit()
