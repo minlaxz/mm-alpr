3cf76738d4ce93d08cc179c8c77af6d6a87b17e6
minlaxz <minminlaxz@gmail.com>
2021-01-28 02:50:42 +0630

    FPS calc.

--- tests/test_opencv_video.py
+++ tests/test_opencv_video.py
@@ -1,18 +1,30 @@
 import numpy as np
-import cv2
+import cv2, time
+import multiprocessing as m
 
-cap = cv2.VideoCapture(f'rtsp://192.168.0.146:8080/h264_pcm.sdp')
+# cap = cv2.VideoCapture(f'rtsp://192.168.0.146:8080/h264_pcm.sdp')
 
-while(True):
-    # Capture frame-by-frame
+cap = cv2.VideoCapture(0)
+if not cap.isOpened(): exit(1)
+
+    
+count = 0
+prev = time.time()
+while True:
+    count += 1
     ret, frame = cap.read()
+    if not ret: break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    now = time.time()
+    if (now-prev) > 1 :
+        # cv2.putText(gray, 'Current FPS : {}'.format(1 / (now - prev)), (5, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+        print('Avg FPS : {}'.format(count / (now - prev)))
+        count = 0
+        prev = time.time()
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("frame", gray)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 # When everything done, release the capture
