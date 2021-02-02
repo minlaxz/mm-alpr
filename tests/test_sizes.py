29ec24d95dc41c878d420e3254d537fd5cb99417
minlaxz <minminlaxz@gmail.com>
2021-02-02 13:11:01 +0630

    compressing object size

---
+++ tests/test_sizes.py
@@ -0,0 +1,15 @@
+import pickle
+import cv2
+import time
+import sys
+from pympler import asizeof
+
+img = cv2.imread('predicted.jpg')
+
+sys.getsizeof(img, default)
+
+len(pickle.dumps(img))
+
+asizeof.asizeof(img)
+
+
